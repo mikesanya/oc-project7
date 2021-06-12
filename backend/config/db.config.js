e9737cc4d370c1de4362624db49370db412e99a4
unknown <mike_sanya@yahoo.co.uk>
2021-06-12 17:24:42 +0100

    modified

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,2 +1,2 @@
 const Sequelize = require("sequelize");
-module.exports = new Sequelize("groupomania","root","ThankyouLord234",{host:"localhost", dialect:"mysql"});
+module.exports = new Sequelize("groupware","root","ThankyouLord234",{host:"localhost", dialect:"mysql"});
